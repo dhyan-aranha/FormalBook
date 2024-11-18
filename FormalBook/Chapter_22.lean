45f942fb327e21e430c5c480f047db485c8ac470
Pjotr Buys <pjotr.buys@gmail.com>
2024-11-18 18:28:26 +0100

    Added some preamble. Introduced local notation for R^2. Wrote a suggestion for the statement of the main theorem (Monsky).

--- FormalBook/Chapter_22.lean
+++ FormalBook/Chapter_22.lean
@@ -13,10 +13,24 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-Authors: Moritz Firsching, Jan, Maris, Casper, Pjotr, Giacomo, Dion, Thijs, Thomas,
-  Jonas, Dhyan, Lenny.
+Authors: Moritz Firsching,
+Jan,
+Maris Ozols,
+Casper,
+Pjotr Buys,
+Giacomo,
+Dion Leijnse,
+Thijs,
+Thomas Koopman,
+Jonas van der Schaaf,
+Dhyan,
+Lenny Taelman.
 -/
 import Mathlib.Tactic
+import Mathlib.Analysis.InnerProductSpace.PiL2
+
+open BigOperators
+
 /-!
 # One square and an odd number of triangles
 
@@ -40,3 +54,41 @@ import Mathlib.Tactic
     - Claim
     - Zorn's Lemma
 -/
+
+local notation "ℝ²" => EuclideanSpace ℝ (Fin 2)
+
+structure Triangle :=
+  x : ℝ²
+  y : ℝ²
+  z : ℝ²
+
+def closed_triangle (T : Triangle) : Set ℝ² :=
+  { w | ∃ (α₁ α₂ α₃ : ℝ),
+      0 ≤ α₁ ∧
+      0 ≤ α₂ ∧
+      0 ≤ α₃ ∧
+      α₁ + α₂ + α₃ = 1 ∧
+      w = (α₁) • (T.x) + (α₂) • (T.y) + (α₃) • (T.z)}
+
+def open_triangle (T : Triangle) : Set ℝ² :=
+  { w | ∃ (α₁ α₂ α₃ : ℝ),
+      0 < α₁ ∧
+      0 < α₂ ∧
+      0 < α₃ ∧
+      α₁ + α₂ + α₃ = 1 ∧
+      w = (α₁) • (T.x) + (α₂) • (T.y) + (α₃) • (T.z)}
+
+noncomputable def triangle_area (T : Triangle) : ℝ :=
+  abs (- (T.x 1) * (T.y 0) + (T.x 0) * (T.y 1) + (T.x 1) * (T.z 0) - (T.y 1) * (T.z 0) - (T.x 0) * (T.z 1) + (T.y 0) * (T.z 1)) / 2
+
+def is_equal_area_cover (X : Set ℝ²) (S : Set Triangle) : Prop :=
+  (X = ⋃ (T ∈ S), closed_triangle T) ∧
+  (Set.PairwiseDisjoint S open_triangle) ∧
+  ∀ {T₁ T₂}, T₁ ∈ S → T₂ ∈ S → triangle_area T₁ = triangle_area T₂
+
+def unit_square : Set ℝ² := {x : ℝ² | 0 ≤ x 0 ∧ x 0 ≤ 1 ∧ 0 ≤ x 1 ∧ x 1 ≤ 1}
+
+theorem Monsky (n : ℕ):
+    (∃ (S : Finset Triangle), is_equal_area_cover unit_square S ∧ S.card = n)
+    ↔ (n ≠ 0 ∧ Even n) := by
+  sorry
