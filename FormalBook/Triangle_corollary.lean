00c42afdbca25560a0c67f448a2c5fc410e40675
thijsmpm <thijsmaessen19@gmail.com>
2025-01-10 14:25:23 +0100

    Cleaned up some stuff, showed area of triangles is scaled by their determinant, and that the area of closed segment is zero

--- FormalBook/Triangle_corollary.lean
+++ FormalBook/Triangle_corollary.lean
@@ -92,8 +92,8 @@ def unit_segment : Segment := fun | 0 => (v 0 0) | 1 => (v 1 0)
 lemma unit_triangle_def : unit_triangle = fun | 0 => (v 0 0) | 1 => (v 1 0) | 2 => (v 0 1) := by rfl
 lemma unit_segment_def : unit_segment = fun | 0 => (v 0 0) | 1 => (v 1 0)  := by rfl
 
-def open_unit_triangle := open_hull unit_triangle
-def closed_unit_triangle := closed_hull unit_triangle
+--def open_unit_triangle := open_hull unit_triangle
+--def closed_unit_triangle := closed_hull unit_triangle
 
 --first the statement that for a polygon P and a linear map L,  L( open hull P) = open hull (L P),
 --starting with an elementary used to prove it
@@ -194,9 +194,16 @@ theorem translation_commutes_closed {n : ℕ }(f : (Fin n → ℝ²)) (b : ℝ²
     exact ⟨ a, h1, by dsimp ; rwa[ aux_for_translation_closed h1]⟩
 
 --These maps tell us hows to transform from the unit triangle to the an arbitrary triangle
-def matrix_transform ( T : Triangle) : Matrix (Fin 2) (Fin 2) ℝ :=![ ![T 1 0 - T 0 0, T 2 0 - T 0 0], ![T 1 1 - T 0 1, T 2 1 - T 0 1]]
-def linear_transform (T : Triangle) := Matrix.toLin' (matrix_transform T)
+noncomputable def our_basis : Basis (Fin 2) ℝ ℝ² :=  PiLp.basisFun 2 ℝ (Fin 2)
+noncomputable def basis_transform (T: Triangle ) : (Fin 2 → ℝ²) := (fun | 0 => (T 1 - T 0) | 1 => (T 2 -T 0)) --I think I was forced to use this definition to ensure it is the correct type
+noncomputable def linear_transform (T : Triangle) := our_basis.constr ℝ (basis_transform T)
+-- def matrix_transform ( T : Triangle) : Matrix (Fin 2) (Fin 2) ℝ :=![ ![T 1 0 - T 0 0, T 2 0 - T 0 0], ![T 1 1 - T 0 1, T 2 1 - T 0 1]]
+-- def linear_transform1 (T : Triangle)  := Matrix.toLin' (matrix_transform T) --Have to specify type or else it well think it to be for the non euclidean space
 def triangle_translation (T : Triangle) := translation (T 0)
+theorem our_basis_def : our_basis = PiLp.basisFun 2 ℝ (Fin 2) := by rfl
+theorem basis_transform_def (T: Triangle) : basis_transform T =  (fun | 0 => (T 1 - T 0) | 1 => (T 2 -T 0)) := by rfl
+theorem linear_transform_def (T : Triangle) : linear_transform T =  our_basis.constr ℝ (basis_transform T) := by rfl
+theorem triangle_translation_def (T : Triangle ): triangle_translation T =  translation (T 0) := by rfl
 
 --This theorem tells us that these maps indeed do the trick, combined with translation_commutes and open_hull_lin_trans
 theorem unit_triangle_to_triangle (T : Triangle): Set.image (triangle_translation T) (Set.image (linear_transform T) (open_hull unit_triangle)) = open_hull T:= by
@@ -212,27 +219,121 @@ theorem unit_triangle_to_triangle (T : Triangle): Set.image (triangle_translatio
   apply congrArg
   --This part of the proof says that the linear transformation and translation of the unit triangle give the triangle we want
   ext i j
-  fin_cases i <;> fin_cases j <;> simp[translation,linear_transform, f, matrix_transform, Matrix.mulVec, Matrix.vecHead, Matrix.vecTail  ]
+  fin_cases i <;> fin_cases j <;>  simp[translation, linear_transform, basis_transform,f, our_basis]
+
+
 
 theorem area_lin_map ( L : ℝ² →ₗ[ℝ ]  ℝ²) (A : Set ℝ²) : MeasureTheory.volume (Set.image L A) = (ENNReal.ofReal (abs ( LinearMap.det L ))) * (MeasureTheory.volume (A)) := by
   exact MeasureTheory.Measure.addHaar_image_linearMap MeasureTheory.volume L A
 
 
 --This is some garbage that is not working yet (I want it to be like the above)
- theorem area_translation (a : ℝ²)(A : Set ℝ²) :  MeasureTheory.volume (Set.image (fun x ↦ a + x ) A) = MeasureTheory.volume (A) :=   by sorry
-  -- MeasureTheory.volume.IsAddLeftInvariant
---   sorry
--- variable (a b : (Fin 2) → ℝ)(G : Type) [AddGroup G] [TopologicalSpace G] [TopologicalAddGroup G] [MeasurableSpace G] [BorelSpace G]  (K₀ : TopologicalSpace.PositiveCompacts G)
--- #check MeasureTheory.Measure.isAddLeftInvariant_addHaarMeasure K₀
---#check map_add_left_eq_self
+ theorem area_translation (a : ℝ²)(A : Set ℝ²) :  MeasureTheory.volume (Set.image (translation a) A) = MeasureTheory.volume (A) :=   by sorry
+
+
 
 -- This is what we want to prove
-theorem volume_open_unit_triangle : (MeasureTheory.volume open_unit_triangle).toReal = (1/2 : ℝ ) := by sorry
+
+-- I think it is easiest for now to leave this in ENNReal
+theorem volume_open_unit_triangle : (MeasureTheory.volume (open_hull unit_triangle)) = 1/2 := by sorry
+
+
 
 
 
 theorem volume_open_triangle ( T : Triangle ) : (MeasureTheory.volume (open_hull T)).toReal = triangle_area (T : Triangle) := by
-  sorry
+  rw[← unit_triangle_to_triangle T ,triangle_translation_def]
+  rw[ area_translation, area_lin_map, volume_open_unit_triangle]
+  rw[← Matrix.toLin_toMatrix our_basis our_basis  ( linear_transform T ) ]
+  rw[LinearMap.det_toLin our_basis ((LinearMap.toMatrix our_basis our_basis) (linear_transform T))]
+  rw[Matrix.det_fin_two]
+  rw[linear_transform_def, basis_transform_def, our_basis_def, triangle_area ]
+  repeat rw[LinearMap.toMatrix_apply]
+  simp
+  ring_nf
+
+
+def y_axis : Submodule ℝ ℝ² := Submodule.span ℝ (Set.range unit_segment )
+theorem y_axis_def :  y_axis = Submodule.span ℝ (Set.range unit_segment ) := by rfl
+
+
+theorem closed_unit_segment_subset : closed_hull unit_segment ⊆ y_axis := by
+  intro x
+  rintro ⟨  a ,⟨ h1,h3⟩  , h2⟩
+  rw[y_axis]
+  --this is to get rid of the annoying coercion
+  have h :(x ∈ (Submodule.span ℝ (Set.range unit_segment))) →  x ∈ ↑(Submodule.span ℝ (Set.range unit_segment))
+  intro h1
+  exact h1
+  apply h
+
+  rw[ mem_span_range_iff_exists_fun]
+  use a
+
+-- theorem Ennreal_zero_real_zero (A : ENNReal) :  (A = 0) → A.toReal = 0 := by
+--   intro h
+--   rw[h]
+--   rw [ENNReal.zero_toReal]
+
+--This argument can probably be a lot cleaner
+theorem volume_closed_unit_segment : MeasureTheory.volume (closed_hull unit_segment) = 0 := by
+  apply MeasureTheory.measure_mono_null (closed_unit_segment_subset )
+  apply MeasureTheory.Measure.addHaar_submodule
+  intro h
+  have h3 : (fun | 0 => 0 | 1 => 1) ∉ y_axis
+  intro h1
+  rw[y_axis] at h1
+  rw[ mem_span_range_iff_exists_fun] at h1
+  cases' h1 with c h1
+  rw[Fin.sum_univ_two, unit_segment_def] at h1
+  simp at h1
+  apply congrFun at h1
+  specialize h1 1
+  dsimp at h1
+  have h2 : c 0 * 0 + c 1 * 0 = 0
+  linarith
+  rw[h2] at h1
+  apply zero_ne_one at h1
+  exact h1
+  rw[h] at h3
+  apply h3
+  trivial
+
+
+noncomputable def basis_transform_segment (L: Segment ) : (Fin 2 → ℝ²) := (fun | 0 => (L 1 - L 0) | 1 => 0)
+noncomputable def linear_transform_segment (L : Segment) := our_basis.constr ℝ (basis_transform_segment L)
+
+def segment_translation (L : Segment) := translation (L 0)
+theorem basis_transform_segment_def (L : Segment)  : basis_transform_segment L =  (fun | 0 => (L 1 - L 0) | 1 => 0) := by rfl
+theorem linear_transform_segment_def (L : Segment) : linear_transform_segment L =  our_basis.constr ℝ (basis_transform_segment L) := by rfl
+theorem segment_translation_def (L : Segment) : segment_translation L =  translation (L 0) := by rfl
+
+theorem unit_segment_to_segment ( L : Segment) : Set.image (segment_translation L) (Set.image (linear_transform_segment L) (closed_hull unit_segment)) = closed_hull L := by
+  have h1 : segment_translation L = translation (L 0) := by rfl
+  let f : (Fin 2 → ℝ²) := fun | 0 => (v 0 0) | 1 => (v 1 0)
+  have hunit_segment : unit_segment = f :=by rfl
+  rw[hunit_segment, h1]
+  have h2 : closed_hull (linear_transform_segment L ∘ f )= ⇑(linear_transform_segment L) '' closed_hull f
+  exact closed_hull_lin_trans (linear_transform_segment L) f
+  rw[← h2]
+  --rw[← open_hull_lin_trans (linear_transform T) f] Why doesnt this work!??
+  rw[← translation_commutes_closed]
+  apply congrArg
+  --This part of the proof says that the linear transformation and translation of the unit triangle give the triangle we want
+  ext i j
+  fin_cases i <;> fin_cases j <;> simp[translation, linear_transform_segment, basis_transform_segment,f, our_basis]
+
+
+theorem volume_closed_segment( L : Segment ) : (MeasureTheory.volume (closed_hull L)).toReal = 0 := by
+  rw[←  unit_segment_to_segment L ,segment_translation_def]
+  rw[ area_translation, area_lin_map, volume_closed_unit_segment]
+  rw[← Matrix.toLin_toMatrix our_basis our_basis  ( linear_transform_segment L ) ]
+  rw[LinearMap.det_toLin our_basis ((LinearMap.toMatrix our_basis our_basis) (linear_transform_segment L))]
+  rw[Matrix.det_fin_two]
+  rw[linear_transform_segment_def, basis_transform_segment_def, our_basis_def ]
+  repeat rw[LinearMap.toMatrix_apply]
+  simp
+
 
 -- def point0 : (Fin 2 → ℝ ) := fun | 0 => 0 | 1 => 0
 -- def point1 : (Fin 2 → ℝ ) := fun | 0 => 1 | 1 => 0
@@ -304,72 +405,6 @@ theorem volume_open_triangle ( T : Triangle ) : (MeasureTheory.volume (open_hull
 --   sorry
 --   --rw[ MeasureTheory.MeasurePreserving.map_eq (EuclideanSpace.volume_preserving_measurableEquiv (Fin 2))]
 
-def y_axis : Submodule ℝ ℝ² := Submodule.span ℝ (Set.range unit_segment )
-theorem y_axis_def :  y_axis = Submodule.span ℝ (Set.range unit_segment ) := by rfl
-
-
-theorem closed_unit_segment_subset : closed_hull unit_segment ⊆ y_axis := by
-  intro x
-  rintro ⟨  a ,⟨ h1,h3⟩  , h2⟩
-  rw[y_axis]
-  --this is to get rid of the annoying coercion
-  have h :(x ∈ (Submodule.span ℝ (Set.range unit_segment))) →  x ∈ ↑(Submodule.span ℝ (Set.range unit_segment))
-  intro h1
-  exact h1
-  apply h
-
-  rw[ mem_span_range_iff_exists_fun]
-  use a
-
-theorem Ennreal_zero_real_zero (A : ENNReal) :  (A = 0) → A.toReal = 0 := by
-  intro h
-  rw[h]
-  rw [ENNReal.zero_toReal]
-
---This argument can probably be a lot cleaner
-theorem volume_closed_unit_segment : (MeasureTheory.volume (closed_hull unit_segment)).toReal = 0 := by
-  apply Ennreal_zero_real_zero
-  apply MeasureTheory.measure_mono_null (closed_unit_segment_subset )
-  apply MeasureTheory.Measure.addHaar_submodule
-  intro h
-  have h3 : (fun | 0 => 0 | 1 => 1) ∉ y_axis
-  intro h1
-  rw[y_axis] at h1
-  rw[ mem_span_range_iff_exists_fun] at h1
-  cases' h1 with c h1
-  rw[Fin.sum_univ_two, unit_segment_def] at h1
-  simp at h1
-  apply congrFun at h1
-  specialize h1 1
-  dsimp at h1
-  have h2 : c 0 * 0 + c 1 * 0 = 0
-  linarith
-  rw[h2] at h1
-  apply zero_ne_one at h1
-  exact h1
-  rw[h] at h3
-  apply h3
-  trivial
-
-
-def matrix_transform_segment ( L : Segment) : Matrix (Fin 2) (Fin 2) ℝ :=![ ![L 1 0 - L 0 0, 0], ![L 1 1 - L 0 1, 0]]
-def linear_transform_segment ( L : Segment) := Matrix.toLin' (matrix_transform_segment L)
-def segment_translation ( L : Segment) := translation (L 0)
-
-theorem unit_segment_to_segment ( L : Segment) : Set.image (segment_translation L) (Set.image (linear_transform_segment L) (closed_hull unit_segment)) = closed_hull L := by
-  have h1 : segment_translation L = translation (L 0) := by rfl
-  let f : (Fin 2 → ℝ²) := fun | 0 => (v 0 0) | 1 => (v 1 0)
-  have hunit_segment : unit_segment = f :=by rfl
-  rw[hunit_segment, h1]
-  have h2 : closed_hull (linear_transform_segment L ∘ f )= ⇑(linear_transform_segment L) '' closed_hull f
-  exact closed_hull_lin_trans (linear_transform_segment L) f
-  rw[← h2]
-  --rw[← open_hull_lin_trans (linear_transform T) f] Why doesnt this work!??
-  rw[← translation_commutes_closed]
-  apply congrArg
-  --This part of the proof says that the linear transformation and translation of the unit triangle give the triangle we want
-  ext i j
-  fin_cases i <;> fin_cases j <;> simp[translation,linear_transform_segment, f, matrix_transform_segment, Matrix.mulVec, Matrix.vecHead, Matrix.vecTail  ]
 
 -- theorem segment_subset_affine_space (L : Segment) : closed_hull L ⊆ line[ℝ, (L 0), (L 1)] := by
 --   intro x
@@ -440,118 +475,118 @@ theorem unit_segment_to_segment ( L : Segment) : Set.image (segment_translation 
 
 --We additionally want its flipped version
 
-def flip_unit_triangle : Triangle  := fun | 0 => (v 1 1) | 1 => (v 0 1) | 2 => (v 1 0)
-def open_flip_unit_triangle := open_hull flip_unit_triangle
-def closed_flip_unit_triangle := closed_hull flip_unit_triangle
+-- def flip_unit_triangle : Triangle  := fun | 0 => (v 1 1) | 1 => (v 0 1) | 2 => (v 1 0)
+-- def open_flip_unit_triangle := open_hull flip_unit_triangle
+-- def closed_flip_unit_triangle := closed_hull flip_unit_triangle
 
---Then additionally we have the diagonal
-def diagonal_line : Segment := fun | 0 => (v 1 0) | 1 => (v 0 1)
-def open_diagonal_line := open_hull diagonal_line
+-- --Then additionally we have the diagonal
+-- def diagonal_line : Segment := fun | 0 => (v 1 0) | 1 => (v 0 1)
+-- def open_diagonal_line := open_hull diagonal_line
 
---We now want to show the open_unit_square is the disjoint union of these open triangles
---and the open diagonal
-
-
-def union_of_open_triangles := open_unit_triangle  ∪ open_flip_unit_triangle
+-- --We now want to show the open_unit_square is the disjoint union of these open triangles
+-- --and the open diagonal
 
 
-
-
-theorem open_unit_square1_is_union : open_unit_square1 = union_of_open_triangles ∪  open_diagonal_line := by
-  have hunit : unit_triangle = fun | 0 => (v 0 0) | 1 => (v 1 0) | 2 => (v 0 1) := by rfl
-  have hdiag : diagonal_line = fun | 0 => (v 1 0) | 1 => (v 0 1) := by rfl
-  have hflipunit: flip_unit_triangle = fun | 0 => (v 1 1) | 1 => (v 0 1) | 2 => (v 1 0) := by rfl
-  ext x
-  constructor
-  · rintro ⟨ h,h1,h2, h3 ⟩
-    have h7 := lt_trichotomy (x 0 +x 1) 1
-    rcases h7 with (h4 | h5| h6)
-    · left
-      left
-      use (fun | 0 => (1- x 0 - x 1) | 1 => x 0 | 2 => x 1)
-      constructor
-      · constructor
-        · dsimp
-          intro i
-          fin_cases i <;> dsimp <;> linarith
-        · rw[Fin.sum_univ_three]
-          dsimp
-          linarith
-      · dsimp
-        rw[Fin.sum_univ_three, hunit]
-        simp
-        ext i
-        fin_cases i <;> simp
-    · right
-      use (fun | 0 => x 0 | 1 => x 1 )
-      constructor
-      · constructor
-        · dsimp
-          intro i
-          fin_cases i <;> dsimp <;> linarith
-        · rw[Fin.sum_univ_two]
-          dsimp
-          linarith
-      · dsimp
-        rw[Fin.sum_univ_two,hdiag]
-        simp
-        ext i
-        fin_cases i <;> simp
-    · left
-      right
-      use (fun | 0 => (x 0 + x 1 -1) | 1 => 1 - x 0 | 2 => 1- x 1)
-      constructor
-      · constructor
-        · dsimp
-          intro i
-          fin_cases i <;> dsimp <;> linarith
-        · rw[Fin.sum_univ_three]
-          dsimp
-          linarith
-      · dsimp
-        rw[Fin.sum_univ_three, hflipunit]
-        simp
-        ext i
-        fin_cases i <;> simp
-  · intro h
-    cases' h  with h1 h2
-    cases' h1 with h1 h3
-    · rcases h1 with ⟨ a , ⟨ h4 ,h5⟩ ,h6⟩
-      rw[← h6]
-      dsimp
-      rw[Fin.sum_univ_three,hunit] at *
-      dsimp
-      refine ⟨?_, ?_, ?_, ?_⟩ <;> simp <;> linarith[ h4 0 ,h4 1 ,h4 2, h4 3]
-    · rcases h3 with ⟨ a , ⟨ h4 ,h5⟩ ,h6⟩
-      rw[← h6]
-      dsimp
-      rw[Fin.sum_univ_three,hflipunit] at *
-      dsimp
-      refine ⟨?_, ?_, ?_, ?_⟩ <;> simp <;> linarith[ h4 0 ,h4 1 ,h4 2, h4 3]
-    · rcases h2 with ⟨ a , ⟨ h4 ,h5⟩ ,h6⟩
-      rw[← h6]
-      dsimp
-      rw[Fin.sum_univ_two,hdiag] at *
-      dsimp
-      refine ⟨?_, ?_, ?_, ?_⟩ <;> simp <;> linarith[ h4 0 ,h4 1 ,h4 2, h4 3]
+-- def union_of_open_triangles := open_unit_triangle  ∪ open_flip_unit_triangle
 
 
 
 
-theorem open_unit_squares_are_same : open_unit_square = open_unit_square1 := by
-  ext x
-  constructor
-  · rintro ⟨ a,⟨ ⟨ h2,h3⟩ ,h1⟩ ⟩
-    have hp : Psquare = (fun | 0 => v 0 0 | 1 => v 1 0 | 2 => v 1 1 | 3 => v 0 1) := by rfl
-    rw[← h1]
-    dsimp
-    rw[Fin.sum_univ_four,hp] at *
-    dsimp
-    refine ⟨?_, ?_, ?_, ?_⟩ <;> simp <;> linarith[ h2 0 ,h2 1 ,h2 2, h2 3]
-  · sorry
+-- theorem open_unit_square1_is_union : open_unit_square1 = union_of_open_triangles ∪  open_diagonal_line := by
+--   have hunit : unit_triangle = fun | 0 => (v 0 0) | 1 => (v 1 0) | 2 => (v 0 1) := by rfl
+--   have hdiag : diagonal_line = fun | 0 => (v 1 0) | 1 => (v 0 1) := by rfl
+--   have hflipunit: flip_unit_triangle = fun | 0 => (v 1 1) | 1 => (v 0 1) | 2 => (v 1 0) := by rfl
+--   ext x
+--   constructor
+--   · rintro ⟨ h,h1,h2, h3 ⟩
+--     have h7 := lt_trichotomy (x 0 +x 1) 1
+--     rcases h7 with (h4 | h5| h6)
+--     · left
+--       left
+--       use (fun | 0 => (1- x 0 - x 1) | 1 => x 0 | 2 => x 1)
+--       constructor
+--       · constructor
+--         · dsimp
+--           intro i
+--           fin_cases i <;> dsimp <;> linarith
+--         · rw[Fin.sum_univ_three]
+--           dsimp
+--           linarith
+--       · dsimp
+--         rw[Fin.sum_univ_three, hunit]
+--         simp
+--         ext i
+--         fin_cases i <;> simp
+--     · right
+--       use (fun | 0 => x 0 | 1 => x 1 )
+--       constructor
+--       · constructor
+--         · dsimp
+--           intro i
+--           fin_cases i <;> dsimp <;> linarith
+--         · rw[Fin.sum_univ_two]
+--           dsimp
+--           linarith
+--       · dsimp
+--         rw[Fin.sum_univ_two,hdiag]
+--         simp
+--         ext i
+--         fin_cases i <;> simp
+--     · left
+--       right
+--       use (fun | 0 => (x 0 + x 1 -1) | 1 => 1 - x 0 | 2 => 1- x 1)
+--       constructor
+--       · constructor
+--         · dsimp
+--           intro i
+--           fin_cases i <;> dsimp <;> linarith
+--         · rw[Fin.sum_univ_three]
+--           dsimp
+--           linarith
+--       · dsimp
+--         rw[Fin.sum_univ_three, hflipunit]
+--         simp
+--         ext i
+--         fin_cases i <;> simp
+--   · intro h
+--     cases' h  with h1 h2
+--     cases' h1 with h1 h3
+--     · rcases h1 with ⟨ a , ⟨ h4 ,h5⟩ ,h6⟩
+--       rw[← h6]
+--       dsimp
+--       rw[Fin.sum_univ_three,hunit] at *
+--       dsimp
+--       refine ⟨?_, ?_, ?_, ?_⟩ <;> simp <;> linarith[ h4 0 ,h4 1 ,h4 2, h4 3]
+--     · rcases h3 with ⟨ a , ⟨ h4 ,h5⟩ ,h6⟩
+--       rw[← h6]
+--       dsimp
+--       rw[Fin.sum_univ_three,hflipunit] at *
+--       dsimp
+--       refine ⟨?_, ?_, ?_, ?_⟩ <;> simp <;> linarith[ h4 0 ,h4 1 ,h4 2, h4 3]
+--     · rcases h2 with ⟨ a , ⟨ h4 ,h5⟩ ,h6⟩
+--       rw[← h6]
+--       dsimp
+--       rw[Fin.sum_univ_two,hdiag] at *
+--       dsimp
+--       refine ⟨?_, ?_, ?_, ?_⟩ <;> simp <;> linarith[ h4 0 ,h4 1 ,h4 2, h4 3]
 
 
 
-theorem open_square_union_of : open_unit_square = union_of_open_triangles ∪  open_diagonal_line := by
-  rw[ open_unit_squares_are_same]
-  exact open_unit_square1_is_union
+
+-- theorem open_unit_squares_are_same : open_unit_square = open_unit_square1 := by
+--   ext x
+--   constructor
+--   · rintro ⟨ a,⟨ ⟨ h2,h3⟩ ,h1⟩ ⟩
+--     have hp : Psquare = (fun | 0 => v 0 0 | 1 => v 1 0 | 2 => v 1 1 | 3 => v 0 1) := by rfl
+--     rw[← h1]
+--     dsimp
+--     rw[Fin.sum_univ_four,hp] at *
+--     dsimp
+--     refine ⟨?_, ?_, ?_, ?_⟩ <;> simp <;> linarith[ h2 0 ,h2 1 ,h2 2, h2 3]
+--   · sorry
+
+
+
+-- theorem open_square_union_of : open_unit_square = union_of_open_triangles ∪  open_diagonal_line := by
+--   rw[ open_unit_squares_are_same]
+--   exact open_unit_square1_is_union
